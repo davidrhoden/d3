afa4244789c46b451edceba7e2b65391981286e1
David Rhoden <david@davidrhoden.com>
2016-08-23 09:41:43 -0500

    just not working

--- js/loadmap.js
+++ js/loadmap.js
@@ -1,12 +1,25 @@
-var usgigmap = new Datamap({element: document.getElementById('container'), scope: 'usa'});
-var projection = d3.geo.albersUsa();
-usgigmap.bubbles(anmshows, {
+var usgigmap = new Datamap({element: document.getElementById('container'), scope: 'usa' });
+
+usgigmap.bubbles(anmshows, function (error, data) {
+    var svg = $(".datamap");
+    console.log(svg);
+    svg.selectAll("circles.points")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("transform", function (d) {
+            return "translate(" + projection([d.longitude,d.latitude]) + ")";
+        })
+        .attr("r", 5)
+        .style("fill", "red");
+    }, {
     popupTemplate: function (geo, data) {
     return ['<div class="hoverinfo">' + data.date,
         '<br/>date: ' + data.date + '',
         '<br/>city: ' + data.city + '',
         '<br/>venue: ' + data.venue + '',
-        '</div>'].join('');
+        '</div>'];
+        // '</div>'].join('');
     }
 });
 
